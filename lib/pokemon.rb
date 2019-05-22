c632046e5f5aea44239f4d0321c6591e548944d2
Tony Baker <this.tonybaker@gmail.com>
2019-05-22 23:47:13 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,44 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db, :hp
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type)
+      VALUES (?, ?)
+    SQL
+    db.prepare(sql).execute(name, type)
+    #PREPARE-prepares a statement for execution. Goes in template of prepared statements.
+  end
+
+  def self.find(id, db)
+    sql = <<-SQL
+      SELECT *
+      FROM pokemon
+      WHERE id = ?
+      LIMIT 1
+    SQL
+    rows_array = db.prepare(sql).execute(id)
+    pokemon_array = rows_array.collect do |row|
+      new_pokemon = Pokemon.new(id: id, name: row[1], type: row[2], db: db)
+      new_pokemon.hp = row[3]
+      new_pokemon
+    end
+    pokemon_array[0]
+  end
+
+  # def alter_hp(hp, db) #tests got in way of setting default hp
+  #   sql = <<-SQL
+  #     UPDATE pokemon
+  #     SET hp = ?
+  #     WHERE id = ?
+  #   SQL
+  #   db.prepare(sql).execute(hp, self.id)
+  # end
 end
